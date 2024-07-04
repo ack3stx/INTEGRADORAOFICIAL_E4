9fefcb72e9a85f53a2eb651ba821a955f1eef730
Tomas Villegas <tomasedu1982@gmail.com>
2024-07-04 17:06:25 -0600

    index

--- Views/Login.php
+++ Views/Login.php
@@ -50,7 +50,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand p-2 w-25 h-50 d-inline-block" href="#">
-        <img src="Imagenes/Laguna inn.jpg" alt="Logo" style="width: 180px; height: 80px;" class="rounded-circle rounded-1">
+        <img src="../Imagenes/LOGOHLI.png" alt="Logo" style="width: 180px; height: 80px;" class="rounded-circle rounded-1">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -58,25 +58,25 @@
       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul class="navbar-nav text-center">
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>INICIO</label></a>
+            <a class="nav-link" href="../index.html"><label>INICIO</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>NOSOTROS</label></a>
+            <a class="nav-link" href="nosotros.php"><label>NOSOTROS</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>HABITACIONES</label></a>
+            <a class="nav-link" href="vistahab.php"><label>HABITACIONES</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>SERVICIOS</label></a>
+            <a class="nav-link" href="../index.html #2424"><label>SERVICIOS</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>CONTACTANOS</label></a>
+            <a class="nav-link" href="Contacto.php"><label>CONTACTANOS</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>INICIAR SESION</label></a>
+            <a class="nav-link" href="Login.php"><label>INICIAR SESION</label></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><label>RESERVAR AHORA</label></a>
+            <a class="nav-link" href="Calendario.php"><label>RESERVAR AHORA</label></a>
           </li>
         </ul>
       </div>
