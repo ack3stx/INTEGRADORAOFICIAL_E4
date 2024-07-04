8c1987789b8b84447b4be58799324759060e8bcc
JADD <168865546+y0ADQ1@users.noreply.github.com>
2024-07-04 15:45:27 -0600

    holatodos

--- Views/check2.php
+++ Views/check2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>check 2</title>
-    <link rel="stylesheet" href="../INTEGRADORAOFICIAL_E4/Estilos/check2.css">
+    <link rel="stylesheet" href="../INTEGRADORAOFICIAL_E4/Views/check2.php">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
