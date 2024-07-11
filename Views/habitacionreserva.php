4dd77fc083001743fff042832a4a971596725c83
Etni Castro <etnicastro9@gmail.com>
2024-07-11 15:10:14 -0600

    actually

--- Views/habitacionreserva.php
+++ Views/habitacionreserva.php
@@ -10,8 +10,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../Estilos/habitacionreserva.css">
+<link rel="stylesheet" href="../Estilos/A.css">
 </head>
+<style>
+
+</style>
 <body>
    
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
