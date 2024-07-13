e85e67c49c1ca82513a1d70f24ccbed8d3b058dd
Etni Castro <etnicastro9@gmail.com>
2024-07-13 09:29:54 -0600

    in

--- Views/habitacionreserva.php
+++ Views/habitacionreserva.php
@@ -130,7 +130,9 @@
     <!--TARJETA 1 -->
     <div class="card" style="width: 40rem; flex-wrap:wrap; margin-top:-33%; margin-left:5%">
   <div class="card-body">
- 
+  
+
+    </div>
     <h5 class="card-title">Card title</h5>
     <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
