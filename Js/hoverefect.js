5bde0aaffb3ce5c9f7576c03ce1317862b08e82c
JADD <168865546+y0ADQ1@users.noreply.github.com>
2024-07-13 10:46:33 -0600

    quien es sniper77

--- Js/hoverefect.js
+++ Js/hoverefect.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function applyHoverEffect(event) {
+        event.currentTarget.style.width = '1250px';
+        event.currentTarget.querySelector('h2').style.opacity = '1';
+        event.currentTarget.querySelector('h2').style.bottom = '25px';
+        event.currentTarget.querySelector('h3').style.opacity = '1';
+        event.currentTarget.querySelector('h3').style.bottom = '0';
+    }
+
+    function removeHoverEffect(event) {
+        event.currentTarget.style.width = '350px';
+        event.currentTarget.querySelector('h2').style.opacity = '0';
+        event.currentTarget.querySelector('h2').style.bottom = '0';
+        event.currentTarget.querySelector('h3').style.opacity = '0';
+        event.currentTarget.querySelector('h3').style.bottom = '0';
+    }
+
     function removeEffectsOnSmallScreens() {
         if (window.innerWidth < 769) {
             document.querySelectorAll('.imgBox').forEach(imgBox => {
                 imgBox.style.transition = 'none';
+                imgBox.querySelector('h2').style.transition = 'none';
                 imgBox.querySelector('h3').style.transition = 'none';
                 imgBox.removeEventListener('mouseover', applyHoverEffect);
                 imgBox.removeEventListener('mouseout', removeHoverEffect);
@@ -10,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             document.querySelectorAll('.imgBox').forEach(imgBox => {
                 imgBox.style.transition = 'all 1s ease';
+                imgBox.querySelector('h2').style.transition = 'all 1s ease';
                 imgBox.querySelector('h3').style.transition = 'all 1s ease';
                 imgBox.addEventListener('mouseover', applyHoverEffect);
                 imgBox.addEventListener('mouseout', removeHoverEffect);
@@ -17,28 +35,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function applyHoverEffect(event) {
-        event.currentTarget.style.width = '1250px';
-        event.currentTarget.querySelector('h3').style.opacity = '1';
-        event.currentTarget.querySelector('h3').style.bottom = '25px';
-    }
-
-    function removeHoverEffect(event) {
-        event.currentTarget.style.width = '350px';
-        event.currentTarget.querySelector('h3').style.opacity = '0';
-        event.currentTarget.querySelector('h3').style.bottom = '0';
-    }
+    // Actualizar la URL de redirección
+    const redirectUrl = 'Views/vistahab.php';
 
     document.querySelectorAll('.imgBox').forEach(function(imgBox) {
         imgBox.addEventListener('click', function() {
-            const url = imgBox.getAttribute('data-url');
-            if (url) {
-                window.location.href = url;
-            }
+            window.location.href = redirectUrl;
         });
     });
 
     window.addEventListener('resize', removeEffectsOnSmallScreens);
     removeEffectsOnSmallScreens();
 });
+
+
 
