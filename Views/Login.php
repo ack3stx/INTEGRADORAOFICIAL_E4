48a62abdf4e0d860f1aa3ab20f5606154c73aac1
Etni Castro <etnicastro9@gmail.com>
2024-07-15 13:08:41 -0600

    CAMBIO DE CUANDO INICIA SESION

--- Views/Login.php
+++ Views/Login.php
@@ -52,6 +52,7 @@
     </style>
 </head>
 <body>
+
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top mb-4">
       <div class="container-fluid">
@@ -137,6 +138,7 @@
             </div>
         </div>
     </section>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
