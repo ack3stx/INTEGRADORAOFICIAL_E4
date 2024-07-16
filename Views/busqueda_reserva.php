03360485316d30ee5e18b0183c2aaba20c73941e
Tomas Villegas <tomasedu1982@gmail.com>
2024-07-15 18:48:06 -0600

    actualizaciones_admin_panel

--- Views/busqueda_reserva.php
+++ Views/busqueda_reserva.php
@@ -1,23 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar Example</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../Estilos/estilos_panel_busqueda.css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-
-  <title>Busqueda Clientes</title>
+  <link rel="stylesheet" href="../Estilos/estilos_panel_recepcionista.css">
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger w-100">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container-fluid">
       <a class="navbar-brand" href="panel_recepcionista2.php">Hotel Laguna Inn</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -43,12 +46,31 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="busqueda_huesped.php">
-              <i class="fas fa-bed"></i> Huesped
+              <i class="fas fa-users"></i> Huesped
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="busqueda_empleados.php">
+              <i class="fas fa-bed"></i> Personal
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="reportes_hotel.php">
+              <i class="fas fa-bed"></i> Hotel
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="busqueda_facturacion.php">
+              <i class="fas fa-bed"></i> Facturacion
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="notificaciones.php">
-              <i class="fas fa-bed"></i> Notificaciones
+            <button type="button" class="btn btn-danger position-relative fas fa-envelope">
+  <span class="position-absolute top-0 start-100 translate-middle p-1 bg-success border border-light rounded-circle">
+    <span class="visually-hidden"></span>
+  </span>
+</button>
             </a>
           </li>
         </ul>
@@ -72,55 +94,98 @@
       </div>
     </div>
   </nav>
-  <br>
-  <div class="container d-flex justify-content-center">
-    <form class="d-flex justify-content-center w-100" role="search">
-        <div class="dropdown">
-            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                Estado Reservación
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">Activas</a></li>
-                <li><a class="dropdown-item" href="#">Finalizadas</a></li>
-                <li><a class="dropdown-item" href="#">Canceladas</a></li>
-            </ul>
-        </div>
-        <input class="form-control me-2" type="number" id="checkout" name="checkout" placeholder="Numero De La Reservacion">
+        <div class="container d-flex justify-content-center">
+    <form class="d-flex justify-content-center w-100" role="search" method="post">
+      <select name="estado" id="">
+        <option value="activa">activa</option>
+        <option value="finalizada">finalizada</option>
+        <option value="cancelada">cancelada</option>
+      </select>
+        <input class="form-control me-2" type="number" id="checkout" name="numero" placeholder="Numero De La Reservacion">
         <label class="color-hotel" for="checkin">Fecha inicio:</label>
-        <input class="form-control me-2 width" type="date" id="checkin" name="checkin" style="width: 150px;">
+        <input class="form-control me-2 width" type="date" id="checkin" name="fecha1" style="width: 150px;">
         <label class="color-hotel" for="checkout">Fecha fin:</label>
-        <input class="form-control me-2 width" type="date" id="checkout" name="checkout" style="width: 150px;">
+        <input class="form-control me-2 width" type="date" id="checkout" name="fecha2" style="width: 150px;">
         <button class="btn btn-outline-danger" type="submit">Buscar</button>
     </form>
 </div>
+<?php 
+    include '../Clases/BasedeDatos.php';
+    $conexion = new Database();
+    $conexion->conectarDB();
+    extract($_POST);
+    if($_POST)
+    {
+      if (empty($numero) and empty($fecha1) and empty($fecha2))
+      {
+        $consulta = "select distinct concat(persona.nombre,' ',persona.apellido_paterno,' ',persona.apellido_materno) as Nombre_Huesped, persona.numero_de_telefono,
+reservacion.fecha_,reservacion.estado_reservacion,count(detalle_reservacion.id_detalle_reservacion) as Cantidad_de_habitaciones
+from usuarios
+inner join persona on persona.usuario=usuarios.id_usuario
+inner join huesped on huesped.persona_huesped=persona.id_persona
+inner join reservacion on reservacion.huesped=huesped.id_huesped
+inner join detalle_reservacion on detalle_reservacion.reservacion=reservacion.id_reservacion
+where reservacion.estado_reservacion='$estado'
+group by Nombre, persona.numero_de_telefono,reservacion.fecha_,reservacion.estado_reservacion";
 
-<script>
-        function validarFormulario() {
-            var checkin = document.getElementById('checkin').value;
-            var checkout = document.getElementById('checkout').value;
-            
-            if ((checkin && !checkout) || (!checkin && checkout)) {
-                alert('Por favor, selecciona ambas fechas.');
-                return false; // Evita el envío del formulario
+      }
+      else if (empty($numero))
+      {
+        $consulta = "select distinct concat(persona.nombre,' ',persona.apellido_paterno,' ',persona.apellido_materno) as Nombre_Huesped, persona.numero_de_telefono,
+reservacion.fecha_,reservacion.estado_reservacion,count(detalle_reservacion.id_detalle_reservacion) as Cantidad_de_habitaciones
+from usuarios
+inner join persona on persona.usuario=usuarios.id_usuario
+inner join huesped on huesped.persona_huesped=persona.id_persona
+inner join reservacion on reservacion.huesped=huesped.id_huesped
+inner join detalle_reservacion on detalle_reservacion.reservacion=reservacion.id_reservacion
+where reservacion.fecha_ between '$fecha1' and '$fecha2'
+group by Nombre, persona.numero_de_telefono,reservacion.fecha_,reservacion.estado_reservacion";
+      }
+      else
+      {
+        $consulta = "select distinct concat(persona.nombre,' ',persona.apellido_paterno,' ',persona.apellido_materno) as Nombre_Huesped, persona.numero_de_telefono,
+reservacion.fecha_,reservacion.estado_reservacion,count(detalle_reservacion.id_detalle_reservacion) as Cantidad_de_habitaciones
+from usuarios
+inner join persona on persona.usuario=usuarios.id_usuario
+inner join huesped on huesped.persona_huesped=persona.id_persona
+inner join reservacion on reservacion.huesped=huesped.id_huesped
+inner join detalle_reservacion on detalle_reservacion.reservacion=reservacion.id_reservacion
+where reservacion.id_reservacion=$numero
+group by Nombre, persona.numero_de_telefono,reservacion.fecha_,reservacion.estado_reservacion";
+      }
+
+
+        $tabla = $conexion->seleccionar($consulta);
+
+        echo "
+            <table class='table table-hover'>
+                <thead class='table-dark'>
+                    <tr>
+                    <th>Nombre</th><th>Telefono</th><th>Fecha Reservacion</th><th>Estado Reservacion</th><th>Cantidad Habitaciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+            ";
+            foreach($tabla as $reg)
+            {
+                echo "<tr>";
+                echo "<td> $reg->Nombre_Huesped </td>";
+                echo "<td> $reg->numero_de_telefono </td>";
+                echo "<td> $reg->fecha_ </td>";
+                echo "<td> $reg->estado_reservacion </td>";
+                echo "<td> $reg->Cantidad_de_habitaciones </td>";
+                echo "</tr>";
             }
-            return true; // Permite el envío del formulario
-        }
-    </script>
+            echo "</tbody>";
+            echo "</table>";
+            $conexion->desconectarBD();
 
-  <!-- Optional JavaScript; choose one of the two! -->
+    }
+    ?>
 
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
-
 </html>
