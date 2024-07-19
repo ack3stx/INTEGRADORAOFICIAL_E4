6975382fa8acbfa0ce5fc65c30743fbf1da2f171
JADD <168865546+y0ADQ1@users.noreply.github.com>
2024-07-19 14:02:07 -0600

    cambiobd

--- Clases/BasedeDatos.php
+++ Clases/BasedeDatos.php
@@ -2,8 +2,8 @@
 class Database
 {
     private $PDOLocal;
-    private $user="gaelico";
-    private $password="1234";
+    private $user="root";
+    private $password="";
     private $server="mysql:host=localhost; dbname=INTEGRADORA_ROL_USUARIOSv2";
     function conectarDB()
     {
