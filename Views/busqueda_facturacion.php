e22720adf8e0725c27320109e40c0d330ba8d9d4
Victor Gael <ack3stx10@gmail.com>
2024-07-22 14:52:15 -0600

    añadiendo_info_facturacion

--- Views/busqueda_facturacion.php
+++ Views/busqueda_facturacion.php
@@ -74,6 +74,7 @@
             </a>
           </li>
         </ul>
+        
         <div class="header-right">
           <div class="btn-group">
             <button class="btn btn-danger dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
@@ -94,7 +95,65 @@
       </div>
     </div>
   </nav>
-  
+  <div class="container mt-5">
+        <h2 class="mb-4">Consultar Informacion De La Facturacion</h2>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="nombre">Numero De Reservacion</label>
+                <input name="N_reservacion" type="text" class="form-control" id="nombre" placeholder="Porfavor Ingrese El Numero De La Reservacion.">
+            </div>
+            <button type="submit" class="btn btn-danger">Buscar</button>
+
+        </form>
+    </div>
+    <?php
+
+    
+    include '../Clases/BasedeDatos.php';
+    $db=new Database();
+    $db->conectarDB();
+
+    extract($_POST);
+
+    if (empty($N_reservacion))
+    {
+    
+            }
+    else{
+      $cadena = "CALL Consultar_Informacion_Facturacion('$N_reservacion');";
+
+    $tabla = $db->seleccionar($cadena);
+
+
+    echo "
+            <table class='table table-hover'>
+                <thead class='table-dark'>
+                    <tr>
+                    <th>nombre</th><th>apellido_paterno</th><th>apellido_materno</th><th>RFC</th><th>direccion</th><th>monto_total</th><th>metodo_pago</th>
+                    </tr>
+                </thead>
+                <tbody>
+            ";
+            foreach($tabla as $reg)
+            {
+                echo "<tr>";
+                echo "<td> $reg->nombre </td>";
+                echo "<td> $reg->apellido_paterno </td>";
+                echo "<td> $reg->apellido_materno </td>";
+                echo "<td> $reg->rfc </td>";
+                echo "<td> $reg->direccion </td>";
+                echo "<td> $reg->monto_total </td>";
+                echo "<td> $reg->metodo_pago </td>";
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+
+    }
+            $db->desconectarBD();
+
+            
+          ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
---
+++ procedimientos.sql
@@ -0,0 +1,72 @@
+--PROCEDIMIENTOS
+--------------------------------------------------------------------------------------------------------------------
+-- Este procemidiento nos serviria para poder agregar nuevas habitaciones con su respectivo tipo de habitacion
+
+DELIMITER //
+CREATE PROCEDURE agregar_habitaciones
+(in tipo varchar(25))
+begin
+DECLARE num_habitacion1 int default 122; 
+DECLARE incremento int default 1;
+
+DECLARE num_habitacion2 int default 204;
+
+DECLARE num_habitacion3 int default 303;
+
+declare num_habitaciones int ;
+
+
+
+if tipo=1 then
+select max(num_habitacion) into num_habitaciones  from habitacion WHERE habitacion.num_habitacion LIKE "1%";
+set num_habitaciones =num_habitaciones + incremento;
+insert into habitacion (num_habitacion,piso,estado_habitacion,tipo_habitacion)
+values (num_habitaciones,1,'disponible',tipo);
+end if;
+
+if tipo=2 then
+select max(num_habitacion) into num_habitaciones  from habitacion WHERE habitacion.num_habitacion LIKE "2%";
+set num_habitaciones =num_habitaciones + incremento;
+insert into habitacion (num_habitacion,piso,estado_habitacion,tipo_habitacion)
+values (num_habitaciones,2,'disponible',tipo);
+end if;
+
+if tipo=3 then
+select max(num_habitacion) into num_habitaciones  from habitacion WHERE habitacion.num_habitacion LIKE "3%";
+set num_habitaciones =num_habitaciones + incremento;
+insert into habitacion (num_habitacion,piso,estado_habitacion,tipo_habitacion)
+values (num_habitaciones,3,'disponible',tipo);
+end if;
+
+END //
+DELIMITER ;
+
+ call agregar_habitaciones (1);
+
+
+ ----------------------------------------------------------------------------------------------------------------------
+
+ -- PROCEDIMIENTO PARA ASIGNARLE EL ROL DE HUESPED A UNA PERSONA EN WEB
+
+-- Estre procedimiento toma los datos de nuestro usuario para poder darle de alta dentro de la aplicacion web con su respectivo rol
+DELIMITER //
+
+CREATE PROCEDURE RegistrarUsuarioHuesped(
+IN nombre_usuario VARCHAR(30), 
+IN n_password text,
+IN correo VARCHAR(40))
+BEGIN
+DECLARE usuario_id INT;
+DECLARE rol_huesped INT DEFAULT 1; 
+INSERT INTO Usuarios(nombre_usuario, password, correo)VALUES (nombre_usuario, n_password, correo);
+SET usuario_id = LAST_INSERT_ID();
+INSERT INTO ROL_USUARIO(rol, usuario)
+VALUES (rol_huesped, usuario_id);
+END //
+DELIMITER ;
+ CALL RegistrarUsuarioHuesped('gaelenlinea','contraseña','holamundos1@gmail.com');
+
+ ----------------------------------------------------------------------------------------------------------------------------
+
+
+
