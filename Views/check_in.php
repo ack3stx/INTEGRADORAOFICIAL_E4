d31086bbcf0c86bda4eda681eb4f7b93d635f3bd
Tomas Villegas <tomasedu1982@gmail.com>
2024-08-02 11:22:46 -0600

    ajustes_barra_recepcionista

--- Views/check_in.php
+++ Views/check_in.php
@@ -120,7 +120,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
         <div class="container-fluid">
-          <a class="navbar-brand" href="panel_recepcionista.php">Hotel Laguna Inn</a>
+          <a class="navbar-brand position-absolute top-0 start-0" href="panel_recepcionista.php">Hotel Laguna Inn</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
