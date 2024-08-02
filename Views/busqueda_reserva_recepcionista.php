51b615b1f1810cbbd30e5954d45dc3c131eba415
Tomas Villegas <tomasedu1982@gmail.com>
2024-08-02 12:23:44 -0600

    act_nav_recepcionista

--- Views/busqueda_reserva_recepcionista.php
+++ Views/busqueda_reserva_recepcionista.php
@@ -21,7 +21,7 @@
 ?>
   <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container-fluid">
-      <a class="navbar-brand position-absolute top-0 start-0" href="panel_recepcionista.php">Hotel Laguna Inn</a>
+      <a class="navbar-brand" href="panel_recepcionista.php">Hotel Laguna Inn</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
