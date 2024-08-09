9d614a11f4bff3d8a29e5c3b68082f9567ebc78e
Tomas Villegas <tomasedu1982@gmail.com>
2024-08-09 10:45:28 -0600

    reserva_fisica

--- Scripts/redireccionar.php
+++ Scripts/redireccionar.php
@@ -4,7 +4,12 @@
     $db->conectarDB();
 
     extract($_POST);
-    
+    $consulta="UPDATE DETALLE_PAGO
+SET METODO_PAGO = '$metodo'
+WHERE ID_DETALLE_PAGO = (SELECT MAX(ID_DETALLE_PAGO) FROM DETALLE_PAGO);";
+
+    $db->ejecuta($consulta);
+
     header('Location: ../views/check2.php');
 
 ?>
