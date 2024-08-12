7c4185e14f58edd2ec5a7fcc94e8df1bd928126d
Tomas Villegas <tomasedu1982@gmail.com>
2024-08-11 20:33:46 -0600

    act

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+if(isset($_SESSION["rol"])){
+    $rol=$_SESSION["rol"];
+    switch ($rol) {
+        case 'recepcionista':
+            header("Location:Views/Panel_Recepcionista.php");
+        break;
+        case 'administrador':
+            header("Location:Views/Panel_Admin.php");
+        break;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -56,7 +70,6 @@
             </li>
 
 <?php
-session_start();
 if(isset($_SESSION["usuario"])){
 
   echo ' 
