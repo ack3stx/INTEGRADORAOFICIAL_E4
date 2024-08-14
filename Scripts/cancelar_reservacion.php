21b5b55ff18b2996cb660198360890470ad72f5c
Victor Gael <ack3stx10@gmail.com>
2024-08-14 17:57:09 -0600

    djsandjasdj

--- Scripts/cancelar_reservacion.php
+++ Scripts/cancelar_reservacion.php
@@ -2,15 +2,23 @@
 include '../Clases/BasedeDatos.php';
 $db = new Database();
 $db->conectarDB();
-extract($_POST);
+
+$ID_RESERVACION = $_POST['ID_RESERVACION'];
+$problema = $_POST['problema'];
 
 if (isset($ID_RESERVACION)) {
-    $cancelacion = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` SET `ESTADO_RESERVACION` = 'incoveniente' WHERE `ID_RESERVACION` = $ID_RESERVACION";
-    $problema = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` SET `INCONSISTENCIA` = ':problema' WHERE `ID_RESERVACION` = $ID_RESERVACION"; 
+    $cancelacion = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` 
+                    SET `ESTADO_RESERVACION` = 'incoveniente' 
+                    WHERE `ID_RESERVACION` = $ID_RESERVACION";
+    
+        
+    $problema_sql = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` 
+                     SET `INCONSISTENCIA` = '$problema_escapado' 
+                     WHERE `ID_RESERVACION` = $ID_RESERVACION"; 
 
     try {
         $db->ejecuta($cancelacion);
-        $db->ejecuta($problema);
+        $db->ejecuta($problema_sql);
     } catch (Exception $e) {
         echo "Error al ejecutar la consulta: " . $e->getMessage();
         exit;
@@ -20,4 +28,5 @@ if (isset($ID_RESERVACION)) {
 $db->desconectarBD();
 
 header("Location:../Views/notificaciones.php");
+exit;
 ?>
