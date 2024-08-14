eb0177fc373e1ee0703810c3e4e99f7f7466ac56
Victor Gael <ack3stx10@gmail.com>
2024-08-14 17:48:42 -0600

    fsfkjafdjka

--- Scripts/cancelar_reservacion.php
+++ Scripts/cancelar_reservacion.php
@@ -6,8 +6,11 @@ extract($_POST);
 
 if (isset($ID_RESERVACION)) {
     $cancelacion = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` SET `ESTADO_RESERVACION` = 'incoveniente' WHERE `ID_RESERVACION` = $ID_RESERVACION";
+    $problema = "UPDATE `INTEGRADORA_ROL_USUARIOSv2`.`RESERVACION` SET `INCONSISTENCIA` = ':problema' WHERE `ID_RESERVACION` = $ID_RESERVACION"; 
+
     try {
         $db->ejecuta($cancelacion);
+        $db->ejecuta($problema);
     } catch (Exception $e) {
         echo "Error al ejecutar la consulta: " . $e->getMessage();
         exit;
