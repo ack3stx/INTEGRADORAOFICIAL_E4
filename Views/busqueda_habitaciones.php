30ba78e5b3dc1fcc964b151a7cbed11234c7f28e
Victor Gael <ack3stx10@gmail.com>
2024-08-17 19:57:43 -0600

    dsadads

--- Views/busqueda_habitaciones.php
+++ Views/busqueda_habitaciones.php
@@ -26,7 +26,7 @@
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse text-white" id="navbarSupportedContent">
         <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="busqueda_reserva.php">
