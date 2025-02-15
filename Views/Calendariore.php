33577376addf76b70f4fd20716a0dc9f59c2bf19
Etni Castro <etnicastro9@gmail.com>
2025-02-15 09:11:51 -0600

    CALENDARIO LISTO

--- Views/Calendariore.php
+++ Views/Calendariore.php
@@ -284,8 +284,8 @@ session_start();
                 enable: [
           function(date) {
            
-            return (date.getFullYear() === 2024) || 
-                   (date.getFullYear() === 2025 && date.getMonth() === 0);
+            return (date.getFullYear() === 2025) || 
+                   (date.getFullYear() === 2026 && date.getMonth() === 0);
           }
         ],
 
